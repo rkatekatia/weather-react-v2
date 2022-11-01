f1749c836b9bc35b010336999fc2290737c20127
rkatekatia <39915135+rkatekatia@users.noreply.github.com>
2022-11-01 18:52:38 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
 
-function App() {
-  return <div>Weather app</div>
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather app</h1>
+        <p>
+          <a
+            href="https://github.com/rkatekatia/weather-react-v2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{' '}
+          </a>
+          by Catherine Kariakina
+        </p>
+      </div>
+    </div>
+  )
 }
-
-export default App
