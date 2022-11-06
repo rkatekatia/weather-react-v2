e558bc7e923c7debccaca7e7754f5086ffee2c85
rkatekatia <39915135+rkatekatia@users.noreply.github.com>
2022-11-06 17:32:53 +0100

    added units

---
+++ src/WeatherTemp.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+export default function WeatherTemp(props) {
+  const [unit, setUnit] = useState('celsius')
+  function convertToF(event) {
+    event.preventDefault()
+    setUnit('fahrenheit')
+  }
+  function convertToC(event) {
+    event.preventDefault()
+    setUnit('celsius')
+  }
+  if (unit === 'celsius') {
+    return (
+      <div>
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{' '}
+          <a href="/" onClick={convertToF}>
+            °F
+          </a>
+        </span>
+      </div>
+    )
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32
+    return (
+      <div>
+        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a href="/" onClick={convertToC}>
+            °C
+          </a>{' '}
+          | °F
+        </span>
+      </div>
+    )
+  }
+}
